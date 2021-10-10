51b14bcdbf93b1a11c7aa78586ec9afef62d29f8
herrhu97 <herrhu97@gmail.com>
2021-10-10 16:19:47 +0800

    finish lab1

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,18 @@ public class LeapYear {
             }
         }
     }
+
+    /** test year whether is leap year */
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } 
+        else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } 
+        else {
+            return false;
+        }
+    }
 }
 
